dc7db94ace88e6c2decd1b7fb9c21484b71155fa
gleb-lukicov <165063710+gleb-lukicov@users.noreply.github.com>
2025-01-09 01:41:38 +0000

    main

--- src/be/app/config.py
+++ src/be/app/config.py
@@ -1,18 +1,29 @@
 import os
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
+
+
+class ConfigError(ValueError):
+    """Configuration related errors."""
+
+    def __init__(self, value: str) -> None:
+        super().__init__(f"{value} environment variable is not set")
+
+
+def get_key_from_env(key: str) -> str:
+    """Get a key from the environment variables."""
+    value = os.getenv(key)
+    if not value:
+        raise ConfigError(key)
+
+    return value
 
 
 class Settings(BaseModel):
     """Application configuration"""
-    ALPHA_VANTAGE_KEY: str = Field(..., env='MARKET_API_KEY')
+
+    ALPHA_VANTAGE_KEY: str = get_key_from_env("MARKET_API_KEY")
     API_BASE_URL: str = "https://www.alphavantage.co"
 
     class Config:
         frozen = True
-
-
-# Initialize settings from environment
-settings = Settings(
-    ALPHA_VANTAGE_KEY=os.getenv('MARKET_API_KEY')
-)
